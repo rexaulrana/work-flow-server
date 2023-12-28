a1a465bb3dda38c4de1b63734710ce374361d55e
rexaulrana <rexaul.rana@gmail.com>
2023-12-28 16:32:54 +0600

    task status change by dnd

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
   })
 );
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6rml2ff.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,18 +34,64 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const todoCollection = client.db("workFlowDB").collection("todo");
+    const allTasksCollection = client.db("workFlowDB").collection("allTasks");
 
-    app.get("/todo", async (req, res) => {
-      const filter = req.query;
+    // get all tasks
+    app.get("/allTasks", async (req, res) => {
+      const email = req.query.email;
+      const query = { user: email };
       // console.log(filter);
 
-      const cursor = todoCollection.find();
-      const result = await cursor.toArray();
+      // const cursor = allTasksCollection.find(filter);
+      const result = await allTasksCollection.find(query).toArray();
       res.send(result);
       console.log(result);
     });
+
+    // add task
+    app.post("/allTasks", async (req, res) => {
+      const newTask = req.body;
+      const result = await allTasksCollection.insertOne(newTask);
+      res.send(result);
+      // console.log(result);
+    });
     // Send a ping to confirm a successful connection
+
+    // delete a task
+    app.delete("/allTasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allTasksCollection.deleteOne(query);
+      console.log(query);
+      res.send(result);
+    });
+
+    // update for todo to ongoing  status
+    app.patch("/allTasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateStatus = {
+        $set: {
+          status: "Ongoing",
+        },
+      };
+      const result = await allTasksCollection.updateOne(filter, updateStatus);
+      // console.log(result);
+      res.send(result);
+    });
+    // update for ongoing to completed  status
+    app.patch("/allTasksCom/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateStatus = {
+        $set: {
+          status: "Completed",
+        },
+      };
+      const result = await allTasksCollection.updateOne(filter, updateStatus);
+      // console.log(result);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
